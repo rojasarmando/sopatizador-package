73fa0c747dfb467ecb3056dc63ac6d60067d1c1d
Armando Rojas <armando.develop@gmail.com>
2023-02-15 16:50:32 -0400

    update

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+module.exports = {
+  entry: {
+      app: './src/index.js',
+  },
+  output: {
+    
+    // library: 'Spzdor',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'sopatizador.min.js',
+  },
+  module: {
+    rules: [
+      {
+        
+      test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: [['@babel/preset-env']] }
+        }
+      }
+    ]
+  }
+ 
+};
